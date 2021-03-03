a9cdd71751688364b9f0d21d28141d8618984ea6
cash1000 <77639939+cash1000@users.noreply.github.com>
2021-03-02 21:48:19 -0600

    Add files via upload

--- Frontend.java
+++ Frontend.java
@@ -13,24 +13,29 @@ import java.util.Scanner;
  * // Notes to Grader: n/a
  */
 public class Frontend  {
-    BackendInterfaceHelper backend ;
-    static String[] ratings = {"*0","*1","*2","*3","*4","*5","*6","*7","*8","*9","*10"};
-    List<MovieInterface> topThreeMovies;
-    private  static String[] genres;
+    private BackendInterfaceHelper backend ;// an instance of backend
+    private String[] ratings = {"*0","*1","*2","*3","*4","*5","*6","*7","*8","*9","*10"};// a array of ratings
+    private List<MovieInterface> topThreeMovies;// the top trhee movies
+    private  static String[] genres;//the added genres
 
-    public void run(BackendInterfaceHelper helper){
+    /**
+     * main method that runs all of the front end
+     * @param helper an instance of the backend
+     */
+    public void run(BackendInterfaceHelper helper){//
         this.backend =helper;
-        selectall();
-        this.topThreeMovies =  backend.getThreeMovies(0);
-        List<String> foo = backend.getAllGenres();
-        this.genres = new String[foo.size()];
-        for(int i = 0 ; i < foo.size();i++){
-            genres[i] = foo.get(i);
+        selectall();// priv helpe method to select all ratings
+        this.topThreeMovies =  backend.getThreeMovies(0);// gets the top three
+        List<String> genresTemp = backend.getAllGenres();// gets the genres
+        this.genres = new String[genresTemp.size()];
+        //makes it an array
+        for(int i = 0 ; i < genresTemp.size();i++){
+            genres[i] = genresTemp.get(i);
         }
-        backend.addAvgRating("10");
         boolean exit = false;
-        while(!exit){
+        while(!exit){// while it is not trigered it loops
             int count = 1;
+            //prints a menu of the top three
             for (MovieInterface movie : topThreeMovies) {
                 System.out.print(count + ": ");
                 movieToString(movie);
@@ -41,22 +46,27 @@ public class Frontend  {
             Scanner scan = new Scanner(System.in);
             System.out.println("Select movie by pressing the corresponding number on the key pad or press r to go to ratings or g to go to genre mode or x to exit");
             String res = scan.next();
+            //sees what they want to do
             if(res.toLowerCase().equals("r")){
-                ratingsMenu();
+                ratingsMenu();// goes to the rating menu
             }else if (res.toLowerCase().equals("g")){
-                genreMenu();
+                genreMenu();//goes to the genre menu
             } else if (res.toLowerCase().equals("x")) {
-                exit =true;
+                exit =true;// exits
             } else if((Integer.parseInt(res) > 0) && (Integer.parseInt(res) < 4)){
-                displayMovie(topThreeMovies.get(Integer.parseInt(res) - 1));
+                displayMovie(topThreeMovies.get(Integer.parseInt(res) - 1));// gets a close up of the top three
             }else{
-                System.out.println("respond with correct response");
+                System.out.println("respond with correct response");// prompts them to give a valid response
             }
         }
     }
-    public boolean ratingsMenu() {
-        Scanner scan = new Scanner(System.in);
 
+    /**
+     * runs the main
+     */
+    public void ratingsMenu() {
+        Scanner scan = new Scanner(System.in);
+        //prompts the user
         System.out.println("Select ratings that you want to watch\n A star means it is selected\nPress the number you want to select or deselect\n Enter it in a line separated by spaces");
         for (String rating : ratings) {
             System.out.println(rating);
@@ -64,15 +74,14 @@ public class Frontend  {
         ArrayList<Integer> responses = new ArrayList<>();
         String s = scan.nextLine();
         s = s.replaceAll(" ", "");
-        if(s.length() ==0){
-
-        }else {
+        //gets a list of all of the things they want to deselect or select
+        if(s.length() !=0){
             for(int i = 0 ; i<s.length()-1;i++){
                 responses.add(Integer.parseInt(s.substring(i,i+1)));
             }
             responses.add(Integer.parseInt(s.substring(s.length()-1)));
         }
-
+        //does the deselecting or selecting
         for(int res: responses){
             if(ratings[res].contains("*")){
                 removerating(Integer.toString(res));
@@ -80,10 +89,11 @@ public class Frontend  {
                 addRating(Integer.toString(res));
             }
         }
+        //gets all of the things that match the given critiria
         ArrayList<MovieInterface> matches = new ArrayList<>();
-        for(int i = 0 ; i < 11;i++) {
+        for(int i = 0 ; i < 4;i++) {
             List<MovieInterface> three = backend.getThreeMovies(i*3);
-            for (int j = 0; j < three.size(); j++) {//rremebr to change back
+            for (int j = 0; j < three.size(); j++) {
                 if (!(three.size() == 0)) {
                     matches.add(three.get(j));
                 }
@@ -92,30 +102,11 @@ public class Frontend  {
         System.out.println(backend.getNumberOfMovies()+" is the amount of matches ");
         if(backend.getNumberOfMovies() == 0){
             System.out.println(" Unfortunately there are no matching criteria");
-            return false;
         }
-        return displaygenre(matches);
+         displaylist(matches);
     }
 
-    private boolean displayrat(ArrayList<MovieInterface> foo){
-        boolean exist = false;
-        int start = 0;
-        int pg = 0;
-        Scanner scan = new Scanner(System.in);
-        while (!exist){
-            displayListofRatings(foo,start);
-            System.out.println("page: "+pg);
-            System.out.println("press the page you want to go on or x to exit");
-            Object page = scan.next();
-            if(page.equals("x")){
-                exist= true;
-            }
-            start = 3*(int)page;
-            pg = (int) page;
-        }
-        return false;
-    }
-    public boolean genreMenu() {
+    public void genreMenu() {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Select genre that you want to watch\nA star means it is selected\nPress the number you want to select or deselect\nEnter it in a line separated by spaces");
@@ -155,26 +146,30 @@ public class Frontend  {
         System.out.println(backend.getNumberOfMovies()+" is the amount of matches ");
         if(backend.getNumberOfMovies() == 0){
             System.out.println(" Unfortunately there are no matching criteria");
-            return false;
         }
-       return displaygenre(matches);
+        displaylist(matches);
     }
 
-    private boolean displaygenre(ArrayList<MovieInterface> foo){
-        boolean exist = false;
+    /**
+     *
+     * @param movieList an arraylist of movieInterfaces
+     * @return false when it exits
+     */
+    private boolean displaylist(ArrayList<MovieInterface> movieList){
+        boolean exit = false;
         int start = 0;
         int pg = 0;
         Scanner scan = new Scanner(System.in);
-        while (!exist){
-            displayListofRatings(foo,start);
+        while (!exit){
+            displayListofMovies(movieList,start);// call to private method
             System.out.println("page: "+pg);
             System.out.println("press the page you want to go on or x to exit");
             String page = scan.next();
             if(page.equals("x")){
-                exist= true;
+                exit= true;
                 break;
             }
-            if( 3*Integer.parseInt(page)<foo.size()){
+            if( 3*Integer.parseInt(page)<movieList.size()){
                 start = 3*Integer.parseInt(page);
                 pg = Integer.parseInt(page);
             }else{
@@ -184,28 +179,38 @@ public class Frontend  {
         }
         return false;
     }
-    private void displayListofRatings(ArrayList<MovieInterface> foo,int start){
+
+    /**
+     * displays the movies in a nice way
+     * @param movieList the array list of movies
+     * @param start the begging index
+     */
+    private void displayListofMovies(ArrayList<MovieInterface> movieList,int start){
         String res = "";
-        if(start+1 >= foo.size()){
-            res +="|"+ foo.get(start).getTitle()+prints(foo.get(start).getTitle())+"|\n";
-            res +="|"+ foo.get(start).getDirector()+prints(foo.get(start).getDirector())+"|\n";
-            res +="|"+ foo.get(start).getAvgVote()+prints(Float.toString(foo.get(start).getAvgVote()))+"|\n";
-            res +="|"+ foo.get(start).getGenres()+prints(foo.get(start).getGenres())+"|\n";
-        }else if(start +2>= foo.size()){
-            res +="|"+ foo.get(start).getTitle()+prints(foo.get(start).getTitle())+"|"+foo.get(start+1).getTitle()+prints(foo.get(start+1).getTitle())+"|\n";
-            res +="|"+ foo.get(start).getDirector()+prints(foo.get(start).getDirector())+"|"+foo.get(start+1).getDirector()+prints(foo.get(start+1).getDirector())+"|\n";
-            res +="|"+ foo.get(start).getAvgVote()+prints(Float.toString(foo.get(start).getAvgVote()))+"|"+foo.get(start+1).getAvgVote()+prints(Float.toString(foo.get(start+1).getAvgVote()))+"|\n";
-            res +="|"+ foo.get(start).getGenres()+prints(foo.get(start).getGenres())+"|"+foo.get(start+1).getGenres()+prints(foo.get(start+1).getGenres())+"|\n";
+        if(start+1 >= movieList.size()){
+            res +="|"+ movieList.get(start).getTitle()+prints(movieList.get(start).getTitle())+"|\n";
+            res +="|"+ movieList.get(start).getDirector()+prints(movieList.get(start).getDirector())+"|\n";
+            res +="|"+ movieList.get(start).getAvgVote()+prints(Float.toString(movieList.get(start).getAvgVote()))+"|\n";
+            res +="|"+ movieList.get(start).getGenres()+prints(movieList.get(start).getGenres())+"|\n";
+        }else if(start +2>= movieList.size()){
+            res +="|"+ movieList.get(start).getTitle()+prints(movieList.get(start).getTitle())+"|"+movieList.get(start+1).getTitle()+prints(movieList.get(start+1).getTitle())+"|\n";
+            res +="|"+ movieList.get(start).getDirector()+prints(movieList.get(start).getDirector())+"|"+movieList.get(start+1).getDirector()+prints(movieList.get(start+1).getDirector())+"|\n";
+            res +="|"+ movieList.get(start).getAvgVote()+prints(Float.toString(movieList.get(start).getAvgVote()))+"|"+movieList.get(start+1).getAvgVote()+prints(Float.toString(movieList.get(start+1).getAvgVote()))+"|\n";
+            res +="|"+ movieList.get(start).getGenres()+prints(movieList.get(start).getGenres())+"|"+movieList.get(start+1).getGenres()+prints(movieList.get(start+1).getGenres())+"|\n";
             System.out.println(res);
         }else{
-            res +="|"+ foo.get(start).getTitle()+prints(foo.get(start).getTitle())+"|"+foo.get(start+1).getTitle()+prints(foo.get(start+1).getTitle())+"|"+foo.get(start+2).getTitle()+prints(foo.get(start+2).getTitle())+"|\n";
-            res +="|"+ foo.get(start).getDirector()+prints(foo.get(start).getDirector())+"|"+foo.get(start+1).getDirector()+prints(foo.get(start+1).getDirector())+"|"+foo.get(start+2).getDirector()+prints(foo.get(start+2).getDirector())+"|\n";
-            res +="|"+ foo.get(start).getAvgVote()+prints(Float.toString(foo.get(start).getAvgVote()))+"|"+foo.get(start+1).getAvgVote()+prints(Float.toString(foo.get(start+1).getAvgVote()))+"|"+foo.get(start+2).getAvgVote()+prints(Float.toString(foo.get(start+2).getAvgVote()))+"|\n";
-            res +="|"+ foo.get(start).getGenres()+prints(foo.get(start).getGenres())+"|"+foo.get(start+1).getGenres()+prints(foo.get(start+1).getGenres())+"|"+foo.get(start+2).getGenres()+prints(foo.get(start+2).getGenres())+"|\n";
+            res +="|"+ movieList.get(start).getTitle()+prints(movieList.get(start).getTitle())+"|"+movieList.get(start+1).getTitle()+prints(movieList.get(start+1).getTitle())+"|"+movieList.get(start+2).getTitle()+prints(movieList.get(start+2).getTitle())+"|\n";
+            res +="|"+ movieList.get(start).getDirector()+prints(movieList.get(start).getDirector())+"|"+movieList.get(start+1).getDirector()+prints(movieList.get(start+1).getDirector())+"|"+movieList.get(start+2).getDirector()+prints(movieList.get(start+2).getDirector())+"|\n";
+            res +="|"+ movieList.get(start).getAvgVote()+prints(Float.toString(movieList.get(start).getAvgVote()))+"|"+movieList.get(start+1).getAvgVote()+prints(Float.toString(movieList.get(start+1).getAvgVote()))+"|"+movieList.get(start+2).getAvgVote()+prints(Float.toString(movieList.get(start+2).getAvgVote()))+"|\n";
+            res +="|"+ movieList.get(start).getGenres()+prints(movieList.get(start).getGenres())+"|"+movieList.get(start+1).getGenres()+prints(movieList.get(start+1).getGenres())+"|"+movieList.get(start+2).getGenres()+prints(movieList.get(start+2).getGenres())+"|\n";
         }
         System.out.println(res);
     }
 
+    /**
+     * displays the movie nicly
+     * @param movie a instance of a MovieInterface
+     */
     private void displayMovie(MovieInterface movie){
         System.out.println(movie.getTitle()+" Directed by " +movie.getDirector());
         System.out.println("Rating: "+movie.getAvgVote());
@@ -213,9 +218,19 @@ public class Frontend  {
         System.out.println("Year: "+movie.getYear());
         System.out.println(movie.getDescription());
     }
+
+    /**
+     * helper method to get the title
+     * @param movie a instance of a MovieInterface
+     */
    private void movieToString(MovieInterface movie){
         System.out.print(movie.getTitle());
     }
+
+    /**
+     * adds the rating
+     * @param rating a string representation of a rating
+     */
     private void addRating(String rating) {
         backend.addAvgRating(rating);
         if(!ratings[Integer.parseInt(rating)].contains("*")) {
@@ -223,45 +238,51 @@ public class Frontend  {
         }
 
     }
+    /**
+     * removes the rating
+     * @param rating a string representation of a rating
+     */
     private void removerating(String rating) {
         backend.removeAvgRating(rating);
         if (ratings[Integer.parseInt(rating)].contains("*")) {
             ratings[Integer.parseInt(rating)] = ratings[Integer.parseInt(rating)];
         }
     }
+    /**
+     * adds the rating
+     * @param gen a string representation of a Genre
+     */
         private void addGenre(String gen) {
             backend.addGenre(genres[Integer.parseInt(gen)]);
             if(!genres[Integer.parseInt(gen)].contains("*")) {
                 genres[Integer.parseInt(gen)] = "*" + genres[Integer.parseInt(gen)];
             }
-
         }
-        private void removeGenre(String gen) {
-            backend.removeGenre(genres[Integer.parseInt(gen)]);
-            if(genres[Integer.parseInt(gen)].contains("*")) {
-                genres[Integer.parseInt(gen)] = genres[Integer.parseInt(gen)];
-            }
-
+    /**
+     * removes the rating
+     * @param gen a string representation of a Genre
+     */
+    private void removeGenre(String gen) {
+        backend.removeGenre(genres[Integer.parseInt(gen)]);
+        if(genres[Integer.parseInt(gen)].contains("*")) {
+            genres[Integer.parseInt(gen)] = genres[Integer.parseInt(gen)];
+        }
     }
 
-    private void sortTopThree(){
-       for(int i = 0 ; i < topThreeMovies.size();i++){
-           for(int j = 0 ; j<topThreeMovies.size();j++){
-               if(topThreeMovies.get(i).compareTo(topThreeMovies.get(j))==-1){
-                   MovieInterface movie = topThreeMovies.get(i);
-                   topThreeMovies.remove(i);
-                   topThreeMovies.add(i, topThreeMovies.get(j));
-                   topThreeMovies.remove(j);
-                   topThreeMovies.add(j, topThreeMovies.get(i));
-               }
-           }
-       }
-    }
+    /**
+     * selects all ratings
+     */
     private void selectall(){
         for(int i  = 0 ; i <11;i++){
             backend.addAvgRating(Double.toString(i));
         }
     }
+
+    /**
+     * helps with formating
+     * @param s a string to input into the format
+     * @return a string with the spaces needed to make it even
+     */
     private String prints(String s){
         String st ="";
          for(int i  = 0 ; i <30-s.length();i++){
@@ -269,7 +290,11 @@ public class Frontend  {
          }
          return st;
     }
-
+    /**
+     * helps with formating
+     * @param s a string  list to input into the format
+     * @return a string with the spaces needed to make it even
+     */
     private String prints(List<String> s){
         String st ="";
         int count =26;
